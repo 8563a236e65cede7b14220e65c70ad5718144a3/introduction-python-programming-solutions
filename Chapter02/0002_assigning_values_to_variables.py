db9c1e263d57edd7dce8db37e505eda419f95083
Warren Jitsing <warren.jitsing@gmail.com>
2020-06-08 15:24:33 +0200

    Variable Assignments
    See: Introduction to Python Programming Chapter 2 p38
    
    Why: Show variable assignment system in Python
    
    How: Python Interpreter
    
    Tags: worked example

---
+++ Chapter02/0002_assigning_values_to_variables.py
@@ -0,0 +1,20 @@
+#Assign variable names
+number = 100
+miles = 1000.0
+name = "Python"
+number
+miles
+name
+
+#Change data type stored in variable
+century = 100
+century
+
+century = "hundred"
+century
+
+#Multiple assignment
+a = b = c = 1
+a
+b
+c
