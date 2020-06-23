6ffae960fd58d294bf079b37c22992374b1d4f54
Warren Jitsing <warren.jitsing@gmail.com>
2020-06-23 20:56:14 +0200

    Finish Chapter 7 Multiple Choice
    See: Introduction to Python Programming Chapter 7 Multiple Choice
    
    Why: Consolidate dictionary operations and methods
    
    How: Python
    
    Tags: multiple choice questions

---
+++ Chapter07/0013_multiple_choice.py
@@ -0,0 +1,39 @@
+"""
+Multiple Choice Questions
+
+1) d
+2) b
+3) a
+4) c
+5) b
+6) b
+7) b
+8) c
+9) d
+10) d
+11) c
+12) a
+13) d
+14) b
+15) a
+16) c
+    count = {}
+    count[(1,2,4)] = 5
+    count[(4,2,1)] = 7
+    count[(1,2)] = 6
+    count[(4,2,1)] = 2
+    count
+    tot = 0
+    for i in count:
+        tot = tot + count[i]
+        print(count[i])
+    print(len(count) + tot)
+
+17) a
+18) b
+19) c
+20) a
+21) b
+22) a
+23) d
+"""
