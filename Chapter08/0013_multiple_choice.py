d931aaeafe64514fc8af394cd506d294a54abd47
Warren Jitsing <warren.jitsing@gmail.com>
2020-06-25 11:46:30 +0200

    Finish Chapter 8 Multiple Choice
    See: Introduction to Python Programming Chapter 8 Multiple Choice
    
    Why: Consolidate tuple and set operations and methods
    
    How: Python
    
    Tags: multiple choice questions

---
+++ Chapter08/0013_multiple_choice.py
@@ -0,0 +1,28 @@
+"""
+Multiple Choice Questions
+1)  b
+2)  a    #AttributeError
+3)  b
+4)  b    #TypeError
+5)  a
+6)  c
+7)  d
+8)  a
+9)  d
+10) b
+11) c
+12) a
+13) a
+14) c
+15) a
+16) b
+17) d   #Type Error
+18) a
+19) b
+20) c
+21) b
+22) b
+23) d
+24) c
+25) a
+"""
