9851d1459adad43f0ae0f8f8c09becc96ff0708e
Warren Jitsing <warren.jitsing@gmail.com>
2020-06-26 18:18:05 +0200

    Finish Chapter 9 Multiple Choice
    See: Introduction to Python Programming Chapter 9 Multiple Choice
    
    Why: Consolidate file operations and methods
    
    How: Python
    
    Tags: multiple choice questions

---
+++ Chapter09/0026_multiple_choice.py
@@ -0,0 +1,31 @@
+"""
+1) b
+2) b
+3) a
+4) d
+5) a
+6) b
+7) a
+for i in range(5):
+    with open("data.txt", "w") as f:
+        if i > 0:
+            break
+    print(f.closed)
+
+8) c
+9) d
+10) a
+11) a
+12) b
+13) a
+14) a
+15) d
+16) c
+17) a
+18) a
+19) d
+20) d
+21) b
+22) a
+23) b
+"""
